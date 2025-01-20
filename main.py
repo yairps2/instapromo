0e4ee7df784116dd518f6120ad82855a8c5a196a
Yair Roitman <roity@Yairs-MacBook-Pro.local>
2025-01-20 19:57:35 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,84 @@
+from instagrapi import Client
+import re
+from sql import add_user
+
+############# SETTINGS #############   שם משתמש וסיסמה 
+# Instagram account login
+email = "agitatormicha@gmail.com"
+password = "72217221Mike8295301"
+
+# Hashtag to search for     האשטאג הרצוי לחיפוש
+hashtag = "zamna"
+
+# Number of new users to find   סבב של כמה חיפושים במכה
+required_new_users = 50
+
+# Recent / Top / Reels posts (options: recent, top, reels)   איפה לחפש בפוסטים אחרונים או בטופ פוסטים או ברילס 
+mode = "reels"
+###################################
+
+try:
+    client = Client()
+    client.login(email, password)
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+#######################################################################
+
+def find_posts_by_hashtag(hashtag: str, num = 3) -> list:
+    try:
+        # Get the hashtag feed
+        if mode == "reels":
+            hashtag_feed = client.hashtag_medias_reels_v1(hashtag, num)
+        elif mode == "recent":
+            hashtag_feed = client.hashtag_medias_recent(hashtag, num)
+        else:
+            hashtag_feed = client.hashtag_medias_top(hashtag, num)
+        return hashtag_feed
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+#######################################################################
+
+def get_usernames_from_posts(posts: list) -> list:
+    usernames = []
+    for post in posts:
+        username = post.user.username
+        if username not in usernames:
+            usernames.append(username)
+    return usernames
+    
+#######################################################################
+
+def get_user_info_from_username(username: str) -> str:
+    try:
+        # Get the user info
+        user_info = client.user_info_by_username(username)
+
+        # Find the email in the bio
+        email = re.findall(r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+", user_info.biography)
+
+        return [user_info.username, user_info.full_name, user_info.follower_count, user_info.biography, user_info.public_email, user_info.contact_phone_number, user_info.bio_links, email]
+    
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+    
+#######################################################################
+
+if __name__ == "__main__":
+    # step 1: find posts by hashtag
+    posts = find_posts_by_hashtag(hashtag, required_new_users)
+
+    # step 2: get usernames from posts
+    users = get_usernames_from_posts(posts)
+
+    # step 3: get user info from username
+    user_infos = []
+    for user in users:
+        print(f"Getting info for user: {user}")
+        user_info = get_user_info_from_username(user)
+        if user_info:
+            user_infos.append(user_info)
+        add_user(user_info)
---
+++ sql.py
@@ -0,0 +1,80 @@
+import psycopg2
+from psycopg2.extras import execute_values
+
+################ SETTINGS #############
+database = "instapromo"
+database_user = "instapromo"
+database_password = "instapromo"
+########################################
+
+def add_user(user_info: list):
+    try:
+        username = user_info[0]
+        full_name = user_info[1]
+        follower_count = user_info[2]
+        biography = user_info[3]
+        public_email = user_info[4]
+        phone = user_info[5]
+        bio_links = [link.url for link in user_info[6]]
+        bio_emails = user_info[7]
+
+        # Ensure bio_links and bio_emails are lists of strings
+        bio_links = [str(link) for link in bio_links]
+        bio_emails = [str(email) for email in bio_emails]
+
+        # Connect to the database
+        conn = psycopg2.connect(
+            dbname=database, user=database_user, password=database_password
+        )
+        cursor = conn.cursor()
+
+        # Check if the username already exists
+        cursor.execute("SELECT id FROM users WHERE username = %s", (username,))
+        result = cursor.fetchone()
+
+        if result:
+            print(f"User with username '{username}' already exists.")
+            return
+
+        # Insert the user into the Users table
+        cursor.execute(
+            """
+            INSERT INTO users (username, full_name, follower_count, bio, public_email, phone)
+            VALUES (%s, %s, %s, %s, %s, %s) RETURNING id
+            """,
+            (username, full_name, follower_count, biography, public_email, phone),
+        )
+        user_id = cursor.fetchone()[0]
+
+        # Insert bio links into the UserBioLinks table
+        if bio_links:
+            bio_links_data = [(user_id, link) for link in bio_links]
+            execute_values(
+                cursor,
+                "INSERT INTO UserBioLinks (user_id, link) VALUES %s",
+                bio_links_data,
+            )
+
+        # Insert bio emails into the UserBioEmails table
+        if bio_emails:
+            bio_emails_data = [(user_id, email) for email in bio_emails]
+            execute_values(
+                cursor,
+                "INSERT INTO UserBioEmails (user_id, email) VALUES %s",
+                bio_emails_data,
+            )
+
+        # Commit the transaction
+        conn.commit()
+        print(f"User '{username}' added successfully.")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        if conn:
+            conn.rollback()
+
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
